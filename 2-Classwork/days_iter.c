926b1d73987248ca080684d9ab0edfe325defddd
SHUBHAM <shubhamkumar50@yahoo.com>
2019-05-24 08:30:06 +0530

    24/5

--- 2-Classwork/days_iter.c
+++ 2-Classwork/days_iter.c
@@ -7,41 +7,113 @@ int main()
 	
 	int n;
 	int iter;
-	
+	int days;
+
 	printf("enter the day 1. sun   2. mon 3. tue 4. wed 5. thu 6. fri 7. sat ");
 	scanf("%d", &n);
 	printf("enter no. of days");
 	scanf("%d", &days);
-	
-	int p=( days % 7 );
-	int iter=n+p;
-
-	switch (iter)
+	int p=( n % 7 );
+	int sum=n+days;
+// int j=p+1;
+/*
+	if ( 1 <= sum && sum <= 7 ) 
 	{
-		case 1: 
-			printf("sun");
-			break;
-		case 2:
-			printf("mon");
-			break;
-		case 3:
-			printf("tue");
-			break;	
-		case 4:
-			printf("wed");
-			break;	
-		case 5:
-			printf("thu");
-			break;
-		case 6:
-			printf("fri");
-			break;
-		case 7:
-			printf("sat");
-			break;
-		default:		
-			break;
+		switch (sum)
+		{
+			case 1: 
+					printf("sun");
+					break;
+			case 2:
+					printf("mon");
+					break;
+			case 3:
+					printf("tue");
+					break;	
+			case 4:
+					printf("wed");
+					break;	
+			case 5:
+					printf("thu");
+					break;
+			case 6:
+					printf("fri");
+					break;
+			case 7:
+					printf("sat");
+					break;
+			default:		
+					break;
+		}
+		exit(1);
 	}
+
+	if ( p == 0 )
+	{
+		switch (n)
+		{
+			case 1: 
+					printf("sun");
+					break;
+			case 2:
+					printf("mon");
+					break;
+			case 3:
+					printf("tue");
+					break;	
+			case 4:
+					printf("wed");
+					break;	
+			case 5:
+					printf("thu");
+					break;
+			case 6:
+					printf("fri");
+					break;
+			case 7:
+					printf("sat");
+					break;
+			default:		
+					break;
+		}
+	}
+	else
+	{
+		
+*/	//	iter=days+(p-1);
+
+			
+		switch (l)
+		{
+			case 1: 
+					printf("sun");
+					break;
+			case 2:
+					printf("mon");
+					break;
+			case 3:
+					printf("tue");
+					break;	
+			case 4:
+					printf("wed");
+					break;	
+			case 5:
+					printf("thu");
+					break;
+			case 6:
+					printf("fri");
+					break;
+			case 7:
+					printf("sat");
+					break;
+			default:		
+					break;
+		}
+
+	}
+
+
+
 	return 0;
 }
 
---
+++ 3-Assignments/pattern_hello.c
@@ -0,0 +1,43 @@
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+	int line,ele,m=0,n,k,a=0,j,p;
+	
+
+//	scanf("%d", &n);
+	
+	for(line=1;line<4;line++)
+	{
+		for(ele=1;ele<=a;ele++)
+		{
+			printf("_");
+		}
+		a=a+1;
+		
+		for(j=line;j<2;j++)
+		{
+			printf("hello");
+		}
+
+		for(k=4;k>m;k--)
+		{
+			printf("_");
+		}
+		m=m+2;
+
+		for(p=0;p<=line;p++)
+		{
+			printf("hello");
+		}
+		printf("\n");
+	}
+		return 0;
+}
+
+
