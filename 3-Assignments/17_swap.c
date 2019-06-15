16b19d454a6092e1977575f7a11f27b0185240c7
SHUBHAM <shubhamkumar50@yahoo.com>
2019-06-15 14:11:23 +0530

    15/6

--- 3-Assignments/17_swap.c
+++ 3-Assignments/17_swap.c
@@ -1,7 +1,13 @@
+/*		TITLE:swap two nos without third variable using pointers.
+		AUTHOR: Shubham Kumar Singh
+		DATE: Fri Jun  15 11:57:41 IST 2019
+		DESCRIPTION: Input: two number from user.
+			     Output: Print the swapped number.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-void swapp(int*, int*);
+void swapp(int *, int *);
 
 int main()
 {
@@ -9,23 +15,31 @@ int main()
 	//clear screen
 	system("clear");
 	
-	//declare vars
-	int *n1, *n2;
+	char op;
+	do
+	{	
+		//declare vars
+		int num1, num2;
 
-	//prompt + read user
-	printf("Enter two nos\n");
-	scanf("%d\n%d", n1, n2);
-	
-	swapp(n1, n2);
-	return 0;
+		//prompt + read user
+		printf("Enter two nos\n");
+		scanf("%d\n%d", &num1, &num2);
+		printf("The first number is %d and 2nd number is %d\n", num1, num2);	
+		swapp(&num1, &num2);
+		printf("after swap The first number is %d and 2nd number is %d\n", num1, num2);	
+		//ask user for loop
+		printf("Do you want to cont\n");
+		scanf("\n%c", &op);
+	}while(op == 'y' | op == 'Y');
+return 0;
 }
 
-void swapp(int *n1, int *n2)
+void swapp(int *num1, int *num2)
 {
-	int *n1, *n2;
-	&n1 = n2;
-	&n2 = n1;
-	printf("%d and %d\n", n1, n2);
+	//we use an XOR bitwise operation to swap as XOR gives 1 for the same bits and zero for different bits
+	*num1 = *num1 ^ *num2;
+	*num2 = *num1 ^ *num2;
+	*num1 = *num1 ^ *num2;
 }
 
 
